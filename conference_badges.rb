03c8316221f273e0c83a9a9a35d025f601854e62
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-31 02:13:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index do |name, number|
-    "Hello, #{name}! You'll be assigned to room #{number}!"
+    puts "Hello, #{name}! You'll be assigned to room #{number}!"
   end
 
 end
