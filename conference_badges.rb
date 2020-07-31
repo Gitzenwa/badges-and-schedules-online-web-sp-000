0fcd7cf9d167f00685cb650544308e3ddf6d4580
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-31 02:29:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each do |x| puts
+  puts assign_rooms(attendees).each do |y| puts y
 end
