7395a5b4d45ec55b992f68c90416206a47808d33
Ezenwa Onukwue <eonukwue@gmail.com>
2020-07-31 02:01:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  array = []
-  attendees.each { |badge|  badges = "Hello, my name is #{badge}." }
-  badges
+  badges = attendees.collect { |name| "Hello, my name is #{name}."  }
 end
 
 def assign_rooms
